acd65ca639eabca3568c2fe0044f51b7081cce72
rociobarra650 <rbarrag2@asu.edu>
2026-02-12 21:30:43 -0500

    Users - Genre Select

--- src/mudb_3_select.sql
+++ src/mudb_3_select.sql
@@ -1,1 +1,7 @@
-/* Select *?
+/* Users who have added Genre 'Pop' songs */
+SELECT DISTINCT U.Username
+FROM MUDB_USER U
+JOIN SONG S ON U.Username = S.Username
+JOIN SONG_GENRE SG ON S.SongID = SG.SongID
+WHERE SG.Genre = 'Pop';
+
