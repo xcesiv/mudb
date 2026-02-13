8a1bf6c7139179276d32d6a867d2d36f2ef9574a
rociobarra650 <rbarrag2@asu.edu>
2026-02-13 12:27:52 -0500

    Select SQL
    Added queries to view all users and count songs per genre.

--- src/mudb_3_select.sql
+++ src/mudb_3_select.sql
@@ -5,3 +5,12 @@ JOIN SONG S ON U.Username = S.Username
 JOIN SONG_GENRE SG ON S.SongID = SG.SongID
 WHERE SG.Genre = 'Pop';
 
+/* View All Users */
+SELECT Username, Role, Email
+FROM MUDB_USER;
+
+/* Count number of songs per Genre */
+SELECT sg.Genre, COUNT(sg.SongID) AS TotalSongs
+FROM SONG_GENRE sg
+GROUP BY sg.Genre
+ORDER BY TotalSongs DESC;
